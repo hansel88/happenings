811b2af14ac50be2e2284183ec06dcf8a6e98b8a
reinole <reinhartsen.oc@gmail.com>
2016-05-06 15:23:05 +0200

    Paa god vei med details

--- js/locations.js
+++ js/locations.js
@@ -12,9 +12,16 @@
 
 init = function(){
 	var chosenLocation = getParameterByName('location');
-    console.log(chosenLocation);
+    
     if(chosenLocation == 'sentrumScene'){
-        $('.details-header').attr("Scentrum Scene");
+        $('.details-header').text("Scentrum Scene");
+        $('.details-image').attr("src", "../assets/images/SentrumScene.jpg");
+        $('.details-info').text("Sentrum Scene er et innendørs konsertlokale på Arbeidersamfunnets plass i Oslo. Sentrum Scene er en del av Rockefeller Music Hall Company og har plass til ca 1750 tilskuere.");
+        //$(events listing);
+    } else if(chosenLocation == 'justisen') {
+        $('.details-header').text("Justisen");
+        $('.details-image').attr("src", "../assets/images/Justisen_3.jpg");
+        $('.details-info').text("Masse info om Justisen dette as.");
     };
 };
     init();
